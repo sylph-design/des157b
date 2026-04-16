ae49b8031bf30b4b6f16650e4df2066021fce65c
Stacyphanstudio <stacy@phan.studio>
2026-04-16 12:47:22 -0700

    finished video

--- studio1/script.js
+++ studio1/script.js
@@ -4,37 +4,44 @@
     let poemWords = document.querySelectorAll("span");
     let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let originalPoem = ["The", "wind", "was", "a", "care-free", "soul", "That", "broke", "the", "chains", "of", "earth"];
-    let wordSelector = document.querySelectorAll("span");
-    let poem = document.querySelector("#poem");
+    let jumbling = [true, true, true, true, true, true, true, true, true, true, true, true];
+
 
     setInterval(wordJumbler(), 1000);
-    setInterval(stopWordJumbler(0), 1000);
-    poemWords[0].addEventListener("onmouseover", () => {stopWordJumbler(0);});
-    poemWords[0].addEventListener(onmouseover, function () {console.log("help")});
 
     function wordJumbler () {
-        for (let i = 0; i < poemWords.length; i++) {
-            let tempWord = poemWords[i].innerHTML;
+        setInterval(() => {
+            for (let i = 0; i < poemWords.length; i++) {
+                let tempWord = poemWords[i].innerHTML;
+                poemWords[i].addEventListener('mouseover', function () {
+                    stopWordJumbler(i);
+                });
+                poemWords[i].addEventListener('mouseout', function () {
+                    jumbling[i] = true;
+                });
 
-            for (let j = 0; j < tempWord.length; j++) {
-                setInterval(() => {
-                    let tempLetter = tempWord[j];
-                    let firstPart = tempWord.substring(0, j);
-                    let lastPart = tempWord.substring(j + 1);
-                    let newLetter = alphabet[Math.floor(Math.random() * 25)];
-                    let newString;
+                if (jumbling[i] === true) {
+                    console.log(jumbling[i]);
+                    for (let j = 0; j < tempWord.length; j++) {
+                        
+                            console.log(jumbling[i]);
+                            let firstPart = tempWord.substring(0, j);
+                            let lastPart = tempWord.substring(j + 1);
+                            let newLetter = alphabet[Math.floor(Math.random() * 25)];
+                            let newString;
 
-                    newString = firstPart + newLetter + lastPart;
-                    tempWord = newString;
-                    poemWords[i].innerHTML = newString;
-                }, Math.floor(Math.random() * 700));
+                            newString = firstPart + newLetter + lastPart;
+                            tempWord = newString;
+                            poemWords[i].innerHTML = newString;
+                        
+                    }
+                }
             }
-        }
+        }, Math.floor(Math.random() * 300));
     }
 
     function stopWordJumbler(wordIndex) {
-        console.log(poemWords[wordIndex].innerHTML);
-        console.log(wordIndex);
         poemWords[wordIndex].innerHTML = `${originalPoem[wordIndex]}`;
+        jumbling[wordIndex] = false;
     }
 })();
