87d185bc6fbcdda0bc7b402774b2168b19d3ee77
Stacyphanstudio <stacy@phan.studio>
2026-04-16 08:27:52 -0700

    e

--- studio1/script.js
+++ studio1/script.js
@@ -3,28 +3,38 @@
 
     let poemWords = document.querySelectorAll("span");
     let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let originalPoem = ["The", "wind", "was", "a", "care-free", "soul", "That", "broke", "the", "chains", "of", "earth"];
+    let wordSelector = document.querySelectorAll("span");
+    let poem = document.querySelector("#poem");
 
-    console.log(poemWords);
-    console.log(poemWords[0].innerHTML);
-
-    wordJumbler();
+    setInterval(wordJumbler(), 1000);
+    setInterval(stopWordJumbler(0), 1000);
+    poemWords[0].addEventListener("onmouseover", () => {stopWordJumbler(0);});
+    poemWords[0].addEventListener(onmouseover, function () {console.log("help")});
 
     function wordJumbler () {
         for (let i = 0; i < poemWords.length; i++) {
             let tempWord = poemWords[i].innerHTML;
 
             for (let j = 0; j < tempWord.length; j++) {
-                let tempLetter = tempWord[j];
-                console.log(tempLetter);
+                setInterval(() => {
+                    let tempLetter = tempWord[j];
+                    let firstPart = tempWord.substring(0, j);
+                    let lastPart = tempWord.substring(j + 1);
+                    let newLetter = alphabet[Math.floor(Math.random() * 25)];
+                    let newString;
 
-                console.log(alphabet[Math.floor(Math.random() * 25)]);
-                tempWord[j] = alphabet[Math.floor(Math.random() * 25)];
-                poemWords[i].innerHTML = tempWord;
+                    newString = firstPart + newLetter + lastPart;
+                    tempWord = newString;
+                    poemWords[i].innerHTML = newString;
+                }, Math.floor(Math.random() * 700));
             }
         }
     }
 
-    function stopWordJumbler () {
-
+    function stopWordJumbler(wordIndex) {
+        console.log(poemWords[wordIndex].innerHTML);
+        console.log(wordIndex);
+        poemWords[wordIndex].innerHTML = `${originalPoem[wordIndex]}`;
     }
 })();
