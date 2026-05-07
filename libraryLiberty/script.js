1988986a91b43eb7ee635e8f6467a10e2d416f5a
Stacyphanstudio <stacy@phan.studio>
2026-05-07 07:30:36 -0700

    library liberty code

--- libraryLiberty/script.js
+++ libraryLiberty/script.js
@@ -2,11 +2,14 @@
     "use strict";
     console.log("reading js");
 
+    var canvas = document.querySelector("#canvas");
+
     // module aliases
     var Engine = Matter.Engine,
         Render = Matter.Render,
         Runner = Matter.Runner,
         Bodies = Matter.Bodies,
+        Constraint = Matter.Constraint,
         Composites = Matter.Composites,
         Composite = Matter.Composite,
         MouseConstraint = Matter.MouseConstraint,
@@ -17,30 +20,43 @@
 
     // create a renderer
     var render = Render.create({
-        element: document.body,
+        element: canvas,
         engine: engine,
         options: {
-            width: 1200,
-            height: 750,
-            showAngleIndicator: true
+            width: 805,
+            height: 405,
+            showAngleIndicator: true,
+            wireframes: false,
+            background: 'transparent'
         }
     });
 
     // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    var stack = Composites.stack(597, 0, 5, 10, 0, 0, function (x, y) {
+        return Bodies.rectangle(x, y, 40, 40);
+    });
+    var ground = Bodies.rectangle(400, 410, 810, 60, { isStatic: true });
     Composite.add(engine.world, [
-        Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-        Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
-        Bodies.rectangle(400, 606, 800, 50.5, { isStatic: true })
+        Bodies.rectangle(400, 0, 800, 10, { isStatic: true }),
+        Bodies.rectangle(800, 100, 5, 600, { isStatic: true }),
+        Bodies.rectangle(0, 100, 10, 600, { isStatic: true }),
+        Bodies.rectangle(400, 400, 805, 5, { isStatic: true })
     ])
 
-    // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB]);
+    // add soft global constraint
+    var body = Bodies.circle(280, 100, 30);
 
-        // add mouse control
+    var constraint = Constraint.create({
+        pointA: { x: 380, y: 20 },
+        bodyB: body,
+        pointB: { x: -10, y: -7 },
+        stiffness: 0.001
+    });
+
+    // add all of the bodies to the world
+    Composite.add(engine.world, [stack, body, constraint]);
+
+    // add mouse control
     var mouse = Mouse.create(render.canvas),
         mouseConstraint = MouseConstraint.create(engine, {
             mouse: mouse,
@@ -65,5 +81,8 @@
 
     // run the engine
     Runner.run(runner, engine);
+
+    gsap.to("h1", { rotation: 360, x: 100, duration: 2});
+    gsap.to("h1", { delay: 2, skewX: -15, duration: 2, yoyo: true, repeat: -1});
 
 })();
